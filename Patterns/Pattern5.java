ca55964fb456294dc8a0fee57dc1403fe4561e1d
Mayank Panchal <indusmk90@gmail.com>
2025-09-18 22:17:23 +0530

    Create Pattern5.java

---
+++ Patterns/Pattern5.java
@@ -0,0 +1,20 @@
+class Pattern5 {
+    public static void main(String[] args) {
+        for(int i = 0;i<5;i++){
+            for(int j = 0;j<=i;j++){
+                System.out.print("  " + (i+1) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+
+  1 
+  2   2 
+  3   3   3 
+  4   4   4   4 
+  5   5   5   5   5 
+
+*/
