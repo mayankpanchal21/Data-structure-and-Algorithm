110d09ad81b6c680a92d6371fe33082e8aec5b42
Mayank Panchal <indusmk90@gmail.com>
2025-09-18 22:01:44 +0530

    Create Pattern2.java

---
+++ Patterns/Pattern2.java
@@ -0,0 +1,20 @@
+class Pattern2 {
+    public static void main(String[] args) {
+        for(int i = 0;i<5;i++){
+            for(int j = 0;j<5;j++){
+                System.out.print(" " + (i+1) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+
+ 1  1  1  1  1 
+ 2  2  2  2  2 
+ 3  3  3  3  3 
+ 4  4  4  4  4 
+ 5  5  5  5  5 
+ 
+*/
